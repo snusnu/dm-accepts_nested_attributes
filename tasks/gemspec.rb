7a56d178ca38a7aa24131609a1331757316bc861
snusnu <gamsnjaga@gmail.com>
2009-04-24 19:26:35 +0200

    removed commented code

--- tasks/gemspec.rb
+++ tasks/gemspec.rb
@@ -1,9 +1,6 @@
 desc "Generate gemspec"
 task :gemspec do |x|
   
-  # # Clean up extraneous files before checking manifest
-  # %x[rake clean]
- 
   %x[rake debug_gem > #{GEM_NAME}.gemspec]
   puts "Successfully created gemspec for #{GEM_NAME}!"
   
