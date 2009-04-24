dd0a4a8944539faba19e18a9c382231559da4716
snusnu <gamsnjaga@gmail.com>
2009-04-24 19:29:41 +0200

    removed fixtures and specs for customized has(n, :through) associations.
    It makes no sense to spec all different options for the association
    macros in integration specs. Rather, it should be specced once and
    for all (in unit specs) that dm-accepts_nested_attributes infers the
    right kind of associations. Think about it, then do it!

--- spec/fixtures/photo.rb
+++
@@ -1,9 +0,0 @@
-class Photo
-  include DataMapper::Resource
-
-  property :id,   Serial
-  property :name, String, :nullable => false
-
-  has n, :tagged_things, :class_name => "Tagging"
-  has n, :tags, :through => :tagged_things
-end
