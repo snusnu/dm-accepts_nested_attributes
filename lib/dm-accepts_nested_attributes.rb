55d6029657234285f2e6885bd0c29748c060bf12
snusnu <gamsnjaga@gmail.com>
2009-05-20 04:40:51 +0200

    removed usage of rubygems

--- lib/dm-accepts_nested_attributes.rb
+++ lib/dm-accepts_nested_attributes.rb
@@ -1,10 +1,4 @@
-# Needed to import datamapper and other gems
-require 'rubygems'
 require 'pathname'
-
-# Add all external dependencies for the plugin here
-gem 'dm-core',          '>=0.9.11'
-gem 'dm-validations',   '>=0.9.11'
 
 require 'dm-core'
 require 'dm-validations'
