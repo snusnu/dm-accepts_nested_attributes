2e89d383c24925f3cf48913ac624b2506edef113
snusnu <gamsnjaga@gmail.com>
2009-05-28 06:29:29 +0200

    no need to expect explicitly ordered spec files

--- tasks/spec.rb
+++ tasks/spec.rb
@@ -8,14 +8,6 @@ begin
   desc 'Run specifications'
   Spec::Rake::SpecTask.new(:spec) do |t|
     t.spec_opts << '--options' << 'spec/spec.opts' if File.exists?('spec/spec.opts')
-    t.spec_files = [
-      'spec/unit/resource_spec.rb',
-      'spec/unit/accepts_nested_attributes_for_spec.rb',
-      'spec/integration/belongs_to_spec.rb',
-      'spec/integration/has_1_spec.rb',
-      'spec/integration/has_n_spec.rb',
-      'spec/integration/has_n_through_spec.rb'
-    ]
  
     begin
       gem 'rcov', '~>0.8'
