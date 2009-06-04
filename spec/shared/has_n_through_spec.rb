04a87ad7d20c8c845cfe084aeffcab801748e301
snusnu <gamsnjaga@gmail.com>
2009-06-04 17:44:20 +0200

    removed unneeded assertions
    * deleting a related resource still not working
    * transactions still not working
    
    * other than that, it seems quite fine

--- spec/shared/has_n_through_spec.rb
+++ spec/shared/has_n_through_spec.rb
@@ -55,8 +55,6 @@ describe "every accessible has(n, :through) association with no reject_if proc",
     Project.all.size.should           == 1
     ProjectMembership.all.size.should == 1
     
-    @person.reload
-    
     @person.projects_attributes = { project.id.to_s => { :id => project.id, :name => 'still dm-accepts_nested_attributes' } }
     @person.save
     
@@ -70,9 +68,7 @@ describe "every accessible has(n, :through) association with no reject_if proc",
   it "should perform atomic commits" do
     
     @person.projects_attributes = { 'new_1' => { :name => nil } } # should fail because of validations
-    @person.projects.should be_empty
     @person.save
-    @person.projects.should be_empty
     
     Person.all.size.should            == 1
     ProjectMembership.all.size.should == 0
@@ -80,9 +76,7 @@ describe "every accessible has(n, :through) association with no reject_if proc",
     
     @person.name = nil # should fail because of validations
     @person.projects_attributes = { 'new_1' => { :name => nil } }
-    @person.projects.should be_empty
     @person.save
-    @person.projects.should be_empty
     
     Person.all.size.should            == 0
     ProjectMembership.all.size.should == 0
@@ -103,7 +97,6 @@ describe "every accessible has(n, :through) association with :allow_destroy => f
     ProjectMembership.all.size.should == 1
     Project.all.size.should           == 1
   
-    @person.reload
     @person.projects_attributes = { '1' => { :id => project.id, :_delete => true } }
     @person.save
     
@@ -125,7 +118,6 @@ describe "every accessible has(n, :through) association with :allow_destroy => t
     ProjectMembership.all.size.should == 1
     Project.all.size.should           == 1
   
-    @person.reload
     @person.projects_attributes = { '1' => { :id => project.id, :_delete => true } }
     @person.save
     
