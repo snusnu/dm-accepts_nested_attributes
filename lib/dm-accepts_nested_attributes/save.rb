8de57d1393c996372f80b49c1089fd671e1d7e13
snusnu <gamsnjaga@gmail.com>
2009-06-13 18:05:44 +0200

    adapt overwritten save behavior to latest dm-core changes

--- lib/dm-accepts_nested_attributes/save.rb
+++ lib/dm-accepts_nested_attributes/save.rb
@@ -10,15 +10,6 @@ module DataMapper
           save_parents(*args) && super
         end
       end
-      
-      def parent_relationships
-        parent_relationships = []
-        relationships.each_value do |relationship|
-          next unless relationship.respond_to?(:resource_for) && relationship.loaded?(self)
-          parent_relationships << relationship
-        end
-        parent_relationships
-      end
  
       def save_parents(*args)
         parent_relationships.all? do |relationship|
