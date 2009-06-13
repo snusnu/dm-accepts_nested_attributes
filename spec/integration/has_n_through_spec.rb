123c70f894c2cbbab65caca7ecafa31b7cb575db
snusnu <gamsnjaga@gmail.com>
2009-06-13 20:26:21 +0200

    refactored shared specs for has(n, :through) associations
    * factored out real shared behavior and run it from
      all examples
    
    * causes 1 failure in "should perform atomic commits"

--- spec/integration/has_n_through_spec.rb
+++ spec/integration/has_n_through_spec.rb
@@ -18,7 +18,8 @@ describe DataMapper::NestedAttributes do
         Person.accepts_nested_attributes_for :projects
         @person = Person.new :name => 'snusnu'
       end
-      
+
+      it_should_behave_like "every accessible has(n, :through) association"
       it_should_behave_like "every accessible has(n, :through) association with no reject_if proc"
       it_should_behave_like "every accessible has(n, :through) association with :allow_destroy => false"
       it_should_behave_like "every accessible has(n, :through) association with a nested attributes reader"
@@ -32,9 +33,11 @@ describe DataMapper::NestedAttributes do
         Person.accepts_nested_attributes_for :projects, :allow_destroy => false
         @person = Person.new :name => 'snusnu'
       end
-      
+
+      it_should_behave_like "every accessible has(n, :through) association"
       it_should_behave_like "every accessible has(n, :through) association with no reject_if proc"
       it_should_behave_like "every accessible has(n, :through) association with :allow_destroy => false"
+      it_should_behave_like "every accessible has(n, :through) association with a nested attributes reader"
       
     end
         
@@ -45,9 +48,11 @@ describe DataMapper::NestedAttributes do
         Person.accepts_nested_attributes_for :projects, :allow_destroy => true
         @person = Person.new :name => 'snusnu'
       end
-      
+
+      it_should_behave_like "every accessible has(n, :through) association"
       it_should_behave_like "every accessible has(n, :through) association with no reject_if proc"
       it_should_behave_like "every accessible has(n, :through) association with :allow_destroy => true"
+      it_should_behave_like "every accessible has(n, :through) association with a nested attributes reader"
       
     end
     
@@ -60,9 +65,11 @@ describe DataMapper::NestedAttributes do
           Person.accepts_nested_attributes_for :projects, :reject_if => lambda { |attrs| true }
           @person = Person.new :name => 'snusnu'
         end
-    
+
+        it_should_behave_like "every accessible has(n, :through) association"
         it_should_behave_like "every accessible has(n, :through) association with a valid reject_if proc"
         it_should_behave_like "every accessible has(n, :through) association with :allow_destroy => false"
+        it_should_behave_like "every accessible has(n, :through) association with a nested attributes reader"
       
       end
                   
@@ -73,9 +80,11 @@ describe DataMapper::NestedAttributes do
           Person.accepts_nested_attributes_for :projects, :reject_if => lambda { |attrs| false }
           @person = Person.new :name => 'snusnu'
         end
-    
+
+        it_should_behave_like "every accessible has(n, :through) association"
         it_should_behave_like "every accessible has(n, :through) association with no reject_if proc"
         it_should_behave_like "every accessible has(n, :through) association with :allow_destroy => false"
+        it_should_behave_like "every accessible has(n, :through) association with a nested attributes reader"
       
       end
     
--- spec/shared/has_n_through_spec.rb
+++ spec/shared/has_n_through_spec.rb
@@ -1,89 +1,93 @@
+describe "every accessible has(n, :through) association", :shared => true do
+
+  it "should allow to update an existing project via Person#projects_attributes" do
+    Person.all.size.should            == 0
+    Project.all.size.should           == 0
+    ProjectMembership.all.size.should == 0
+
+    @person.save
+    project = Project.create(:name => 'dm-accepts_nested_attributes')
+    project_membership = ProjectMembership.create(:person => @person, :project => project)
+
+    Person.all.size.should            == 1
+    Project.all.size.should           == 1
+    ProjectMembership.all.size.should == 1
+
+    @person.projects_attributes = { project.id.to_s => { :id => project.id, :name => 'still dm-accepts_nested_attributes' } }
+    @person.save
+
+    Person.all.size.should            == 1
+    ProjectMembership.all.size.should == 1
+    Project.all.size.should           == 1
+
+    Project.first.name.should == 'still dm-accepts_nested_attributes'
+  end
+
+  it "should perform atomic commits" do
+
+    @person.projects_attributes = { 'new_1' => { :name => nil } } # should fail because of validations
+    @person.save
+
+    Person.all.size.should            == 0 # TODO think more if this should be '1'
+    ProjectMembership.all.size.should == 0
+    Project.all.size.should           == 0
+
+    @person.name = nil # should fail because of validations
+    @person.projects_attributes = { 'new_1' => { :name => nil } }
+    @person.save
+
+    Person.all.size.should            == 0
+    ProjectMembership.all.size.should == 0
+    Project.all.size.should           == 0
+
+  end
+
+end
+
 describe "every accessible has(n, :through) association with a valid reject_if proc", :shared => true do
 
   it "should not allow to create a new project via Person#projects_attributes" do
     Person.all.size.should            == 0
     Project.all.size.should           == 0
     ProjectMembership.all.size.should == 0
-    
+
     @person.save
     Person.all.size.should            == 1
     ProjectMembership.all.size.should == 0
     Project.all.size.should           == 0
-    
+
     @person.projects_attributes = { 'new_1' => { :name => 'dm-accepts_nested_attributes' } }
     @person.save
-    
+
     Person.all.size.should            == 1
     ProjectMembership.all.size.should == 0
     Project.all.size.should           == 0
   end
-  
+
 end
 
 describe "every accessible has(n, :through) association with no reject_if proc", :shared => true do
-  
+
   it "should allow to create a new project via Person#projects_attributes" do
     Person.all.size.should            == 0
     Project.all.size.should           == 0
     ProjectMembership.all.size.should == 0
-    
+
     @person.save
     Person.all.size.should            == 1
     ProjectMembership.all.size.should == 0
     Project.all.size.should           == 0
-    
+
     @person.projects_attributes = { 'new_1' => { :name => 'dm-accepts_nested_attributes' } }
     @person.save
-    
+
     Person.all.size.should            == 1
     ProjectMembership.all.size.should == 1
     Project.all.size.should           == 1
-    
+
     Project.first.name.should == 'dm-accepts_nested_attributes'
   end
-      
-  it "should allow to update an existing project via Person#projects_attributes" do
-    Person.all.size.should            == 0
-    Project.all.size.should           == 0
-    ProjectMembership.all.size.should == 0
-    
-    @person.save
-    project = Project.create(:name => 'dm-accepts_nested_attributes')
-    project_membership = ProjectMembership.create(:person => @person, :project => project)
-    
-    Person.all.size.should            == 1
-    Project.all.size.should           == 1
-    ProjectMembership.all.size.should == 1
-    
-    @person.projects_attributes = { project.id.to_s => { :id => project.id, :name => 'still dm-accepts_nested_attributes' } }
-    @person.save
-    
-    Person.all.size.should            == 1
-    ProjectMembership.all.size.should == 1
-    Project.all.size.should           == 1
-    
-    Project.first.name.should == 'still dm-accepts_nested_attributes'
-  end
-  
-  it "should perform atomic commits" do
-    
-    @person.projects_attributes = { 'new_1' => { :name => nil } } # should fail because of validations
-    @person.save
-    
-    Person.all.size.should            == 0 # TODO think more if this should be '1'
-    ProjectMembership.all.size.should == 0
-    Project.all.size.should           == 0
-    
-    @person.name = nil # should fail because of validations
-    @person.projects_attributes = { 'new_1' => { :name => nil } }
-    @person.save
-    
-    Person.all.size.should            == 0
-    ProjectMembership.all.size.should == 0
-    Project.all.size.should           == 0
-    
-  end
-  
+
 end
 
 describe "every accessible has(n, :through) association with :allow_destroy => false", :shared => true do
