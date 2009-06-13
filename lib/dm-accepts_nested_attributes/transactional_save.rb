e3837feda3596cca1ac004543179bd2cfd8aaedf
snusnu <gamsnjaga@gmail.com>
2009-06-13 18:05:18 +0200

    don't use chainable in client code
    * it should be sufficient that dm-core declared
      the method as chainable

--- lib/dm-accepts_nested_attributes/transactional_save.rb
+++ lib/dm-accepts_nested_attributes/transactional_save.rb
@@ -3,14 +3,10 @@ module DataMapper
     
     module TransactionalSave
       
-      extend DataMapper::Chainable
-      
-      chainable do
-        def save(*args)
-          saved = false
-          transaction { |t| t.rollback unless saved = super }
-          saved
-        end
+      def save(*args)
+        saved = false
+        transaction { |t| t.rollback unless saved = super }
+        saved
       end
       
     end
