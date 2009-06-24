226a5a689e9c2d86bf0a51f5d938a2e46f24313a
snusnu <gamsnjaga@gmail.com>
2009-06-24 06:00:41 +0200

    added hopefully last missing documentation

--- lib/dm-accepts_nested_attributes/transactional_save.rb
+++ lib/dm-accepts_nested_attributes/transactional_save.rb
@@ -3,6 +3,17 @@ module DataMapper
     
     module TransactionalSave
       
+      ##
+      # Overrides @see DataMapper::Resource#save to perform inside a transaction.
+      # The current implementation simply wraps the saving of the complete object tree
+      # inside a transaction and rolls back in case any exceptions are raised,
+      # or any of the calls to
+      #
+      # @see DataMapper::Resource#save returned false
+      #
+      # @return [true, false]
+      #   true if all related resources were saved properly
+      #
       def save(*)
         saved = false
         transaction { |t| t.rollback unless saved = super }
