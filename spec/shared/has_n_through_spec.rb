a07dcefdeca5141b837ecdda1673fb1287c5f343
snusnu <gamsnjaga@gmail.com>
2009-06-25 05:26:08 +0200

    added spec that proves that m2m deletion wipes out all intermediaries
    * this is bad bad behavior and needs to be fixed

--- spec/shared/has_n_through_spec.rb
+++ spec/shared/has_n_through_spec.rb
@@ -121,19 +121,21 @@ describe "every accessible has(n, :through) association with :allow_destroy => t
   
   it "should allow to delete an existing project via Person#projects_attributes" do
     @person.save
-    project = Project.create(:name => 'dm-accepts_nested_attributes')
-    project_membership = ProjectMembership.create(:person => @person, :project => project)
+    project_1 = Project.create(:name => 'dm-accepts_nested_attributes')
+    project_2 = Project.create(:name => 'dm-is-localizable')
+    project_membership_1 = ProjectMembership.create(:person => @person, :project => project_1)
+    project_membership_2 = ProjectMembership.create(:person => @person, :project => project_2)
+    
+    Person.all.size.should            == 1
+    ProjectMembership.all.size.should == 2
+    Project.all.size.should           == 2
+
+    @person.projects_attributes = { '1' => { :id => project_1.id, :_delete => true } }
+    @person.save
     
     Person.all.size.should            == 1
     ProjectMembership.all.size.should == 1
     Project.all.size.should           == 1
-
-    @person.projects_attributes = { '1' => { :id => project.id, :_delete => true } }
-    @person.save
-    
-    Person.all.size.should            == 1
-    ProjectMembership.all.size.should == 0
-    Project.all.size.should           == 0
   end
   
 end
